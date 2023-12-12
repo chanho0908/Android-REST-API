60fd05dfe60eb3507310b8076abf4697fa3fe982
chanho <uni070@naver.com>
2023-12-12 16:59:21 +0900

    2023.12.12

--- node_server/docker-entrypoint.sh
+++ node_server/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 ## docker-entrypoint.sh for node.js
-
+## 컨테이너 내부에서 cmd작업을 할 수 있도록 해주는 파일
+## MySQL 서버가 준비되기 전까지 다음 단계로 진행되지 않음
 ## mysql : Docker Compose에서 지정한 이름
 echo "wait db server"
 dockerize -wait tcp://mysql:3306 -timeout 20s
--- node_server/server.js
+++ node_server/server.js
@@ -1,30 +1,29 @@
-// const mysql = require("mysql2/promise");
+const mysql = require("mysql2/promise");
 
-// const dbConnect = async () => {
-//     try {
-//         const connection = await mysql.createConnection({
-//             host: "mysql",
-//             user: "root",
-//             password: "root",
-//             database: "cloudbridge_database",
-//             port: 3307,
-//         });
+const dbConnect = async () => {
+    try {
+        const connection = await mysql.createConnection({
+            host: "mysql",
+            user: "root",
+            password: "root",
+            database: "cloudbridge_database",
+        });
 
-//         console.log("mysql connection success");
-//     } catch (error) {
-//         console.log(error);
-//     }
-// };
+        console.log("mysql connection success");
+    } catch (error) {
+        console.log(error);
+    }
+};
 
-// dbConnect();
-const express = require('express')
-const app = express()
-const port = 3000;
+dbConnect();
+// const express = require('express')
+// const app = express()
+// const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Docker with nodejs')
-})
+// app.get('/', (req, res) => {
+//   res.send('Docker with nodejs')
+// })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
