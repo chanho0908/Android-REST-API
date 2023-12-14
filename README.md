f791d2ac9f10469e1dc24a4252b23dda79ecdcdf
chanho <uni070@naver.com>
2023-12-14 23:12:43 +0900

    2023.12.14

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # android_node_server
-## 안드로이드 프로젝트에 사용할 Docker + node.js + nginx server
+
+### 안드로이드 프로젝트에 사용할 Docker + node.js + nginx server
--- node_server/server.js
+++ node_server/server.js
@@ -1,29 +1,61 @@
-const mysql = require("mysql2/promise");
+const express = require('express');
+const multer = require('multer');
+const crypto = require('crypto');
+const fs = require('fs');
+const mysql = require('mysql2');
 
-const dbConnect = async () => {
-    try {
-        const connection = await mysql.createConnection({
-            host: "mysql",
-            user: "root",
-            password: "root",
-            database: "cloudbridge_database",
+const app = express();
+
+// 디스크에 이미지 저장을 위한 multer 설정
+const storage = multer.diskStorage({
+    destination: 'uploads/',
+    filename: (req, file, cb) => {
+        crypto.pseudoRandomBytes(16, (err, raw) => {
+            if (err) return cb(err);
+            const fileName = req.body.storename + '_main_img.' + file.originalname.split('.').pop();
+            cb(null, fileName);
         });
-
-        console.log("mysql connection success");
-    } catch (error) {
-        console.log(error);
     }
-};
+});
 
-dbConnect();
-// const express = require('express')
-// const app = express()
-// const port = 3000;
+const upload = multer({ storage: storage });
 
-// app.get('/', (req, res) => {
-//   res.send('Docker with nodejs')
-// })
+// MySQL 연결 설정
+const connection = mysql.createConnection({
+    host: 'mysql',
+    user: 'root',
+    password: 'root',
+    database: 'cloudbridge_database'
+});
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+app.post("/db/postImg", upload.single('storeimage'), (req, res) => {
+    try {
+        const file = req.file;
+        const storename = req.body.storename;
+
+        if (file) {
+            const filePath = file.path;
+
+            connection.query(
+                'INSERT INTO STORE_IMAGES (STORENAME, IMAGE_PATH) VALUES (?, ?)',
+                [storename, filePath],
+                (err, results, fields) => {
+                    if (!err) {
+                        console.log("이미지가 성공적으로 저장되었습니다.");
+                        res.send("이미지가 성공적으로 저장되었습니다.");
+                    } else {
+                        console.error("이미지 저장 실패: ", err);
+                        res.send("이미지 저장 실패");
+                    }
+                }
+            );
+        }
+    } catch (err) {
+        console.error(err);
+        res.send("오류 발생");
+    }
+});
+
+app.listen(3000, () => {
+    console.log("서버가 3000 포트에서 실행 중입니다.");
+});
